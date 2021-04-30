337e7bf851bfb92e0f388ff1ffe97ab09678bf1d
XioRi007 <67315410+XioRi007@users.noreply.github.com>
2021-04-30 15:42:13 +0300

    Delete vstup directory

--- vstup/db/db.config.js
+++
@@ -1,9 +0,0 @@
-
-
-const nano = require('nano')('http://admin:1234@localhost:5984');
-//если нет такой базы - создать 
-const vstup = nano.db.use('vstup');
-const users = nano.db.use('users');
-const tools = nano.db.use('tools');
-//возможно даже заполнить
-module.exports ={vstup, users, tools, nano}
--- vstup/install.bat
+++
@@ -1,1 +0,0 @@
-npm install && npm install -g react-scripts && npm run client:install && npm run client:build -y
