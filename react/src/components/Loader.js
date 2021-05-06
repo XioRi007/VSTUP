f206eabf5aac59ac6883dc61e50fec5eac8ea5a4
XioRi007 <67315410+XioRi007@users.noreply.github.com>
2021-05-06 16:47:57 +0300

    Delete react directory

--- react/src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Loader = () => (
-  <div style={{display: 'flex', justifyContent: 'center', paddingTop: '2rem'}}>
-    <div className="spinner-border text-primary">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-  </div>
-)
--- react/src/context/timeContext.js
+++
@@ -1,7 +0,0 @@
-import {createContext} from 'react'
-
-function noop() {}
-
-export const TimeContext = createContext({
-  timeIsGone:false, setTime:noop
-});
