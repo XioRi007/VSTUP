fcbc839d899549bf59d3b47d6fa6e9d36115fc21
XioRi007 <67315410+XioRi007@users.noreply.github.com>
2021-05-07 08:27:24 +0300

    Delete server directory

--- server/db/db.config.js
+++
@@ -1,5 +0,0 @@
-const nano = require('nano')('http://admin:1234@couch:5984');
-const vstup = nano.db.use('vstup');
-const users = nano.db.use('users');
-const tools = nano.db.use('tools');
-module.exports ={vstup, users, tools, nano}
