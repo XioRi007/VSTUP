1240b79495b4b8aab4ba588e1fe04892a4a92543
XioRi007 <67315410+XioRi007@users.noreply.github.com>
2021-05-07 08:27:39 +0300

    Delete client directory

--- client/src/context/auth/authContext.js
+++
@@ -1,7 +0,0 @@
-import {createContext} from 'react'
-
-function noop() {}
-
-export const AuthContext = createContext({
-  isAdmin:false, isApplicant:false, adminLogin:noop, adminLogout:noop, logout:noop, login:noop, userId:null, ready:false
-});
--- client/src/context/toolsContext.js
+++
@@ -1,8 +0,0 @@
-import {createContext} from 'react'
-
-function noop() {}
-
-export const ToolsContext = createContext({
-  timeIsGone:false, setTime:noop,
-  maxNumberOfApplications:5, changeMaxNum:noop
-});
