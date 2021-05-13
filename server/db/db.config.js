f0c480a27b980558e0e9f932f0de18cd0f2e037f
XioRi007 <67315410+XioRi007@users.noreply.github.com>
2021-05-13 16:01:48 +0300

    Update db.config.js

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -1,4 +1,5 @@
-const nano = require('nano')('http://admin:1234@couch:5984');
+const {USER, PASSWORD} = require('../config');
+const nano = require('nano')(`http://${USER}:${PASSWORD}@couch:5984`);
 const vstup = nano.db.use('vstup');
 const users = nano.db.use('users');
 const tools = nano.db.use('tools');
